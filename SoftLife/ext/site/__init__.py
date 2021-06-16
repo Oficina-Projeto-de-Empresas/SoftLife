e8ef91980a8f851468071a15b26c5df00ed1c3b6
vinicius tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2021-06-16 07:57:15 -0300

    created new blueprint and added error handler

--- SoftLife/ext/site/__init__.py
+++ SoftLife/ext/site/__init__.py
@@ -1,5 +1,22 @@
-from .main import bp
+from flask import render_template
+
+from .main import bp as main_bp
+from .products import bp as products_bp
+from .feed import bp as feed_bp
+from SoftLife.forms.form_contact import ContactForm
+from SoftLife.forms.form_auth_user import LoginForm, RegistrationForm
 
 def init_app(app):
-    app.register_blueprint(bp)
+    app.register_blueprint(main_bp)
+    app.register_blueprint(products_bp, url_prefix='/produtos')
+    app.register_blueprint(feed_bp, url_prefix='/feed')
 
+    @app.errorhandler(404)
+    def not_found(e):
+        form_contact = ContactForm()
+        form_login = LoginForm()
+        form_registration = RegistrationForm()
+        return render_template('page_404.html',
+                                form_contact=form_contact, 
+                                form_login=form_login, 
+                                form_registration=form_registration), 404
