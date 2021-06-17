df0837c70548fe57f0e708ecc69c2bbe2b46df81
vinicius tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2021-06-17 09:13:22 -0300

    Created route and product template

---
+++ SoftLife/ext/site/products.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from werkzeug.urls import url_parse
+
+
+from SoftLife.forms.form_contact import ContactForm
+from SoftLife.forms.form_auth_user import LoginForm, RegistrationForm
+
+from ..mail import Message, mail
+
+bp = Blueprint('Produtos', __name__)
+
+
+
+@bp.route('/', methods=['GET', 'POST'])
+def products():
+    form_contact = ContactForm()
+    form_login = LoginForm()
+    form_registration = RegistrationForm()
+    return render_template('products.html', title='Produtos',
+                            form_contact=form_contact, 
+                            form_login=form_login, 
+                            form_registration=form_registration)
+
+@bp.route('/<int:id>', methods=['GET', 'POST'])
+def products_id(id):
+    return f"tela {id} produtos"
