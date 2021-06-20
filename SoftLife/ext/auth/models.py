7d15729a83e8d5a6176d1268678f30f8f408c4c3
vinicius tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2021-06-20 05:37:07 -0300

    Resolving user authentication and registration error

--- SoftLife/ext/auth/models.py
+++ SoftLife/ext/auth/models.py
@@ -12,14 +12,16 @@ class User(UserMixin, db.Model):
     cpf = db.Column("cpf", db.String(11), unique = True, nullable=False)
     telephone = db.Column("telephone", db.String(11))
     email = db.Column("email", db.Unicode, unique=True, nullable=False)
-    passwd = db.Column("passwd", db.Unicode, nullable=False)
+    password = db.Column("password", db.Unicode, nullable=False)
     admin = db.Column("admin", db.Boolean)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    def __init__(self, name, cpf, telephone, admin, email, password):
+        self.name = name
+        self.cpf = cpf
+        self.telephone = telephone
+        self.admin = admin
+        self.email = email
+        self.password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return self.email
+    def verify_password(self, pwd):
+        return check_password_hash(self.password, pwd)
